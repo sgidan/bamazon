58721f3505f28e25c5b514230090d35b31ed72d4
sgidan <sarahgidan@gmail.com>
2019-07-16 14:32:06 -0700

    added code for first prompts--working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,20 +25,40 @@ function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.table(res);
-        connection.end();
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the ID of the product you would like to buy?",
+                    name: "request_id"
+                },
+                {
+                    type: "input",
+                    message: "How many units of the product would you like to buy?",
+                    name: "request_units"
+                }
+            ])
+
+            .then(function (inquirerResponse) {
+                console.log(inquirerResponse)
+                var custReqUnit = inquirerResponse.request_units;
+                var custReqId = inquirerResponse.request_id;
+
+                connection.query("SELECT stock_quantity FROM products WHERE item_id=?", [custReqId], function (err, res) {
+                    if (err) throw err;
+                    var currentSQ = res[0].stock_quantity;
+                    console.log(custReqUnit)
+                    console.log(currentSQ)
+                    if (custReqUnit > currentSQ) {
+                        console.log("Insufficient Quantity!")
+                    }
+
+                });
+                connection.end();
+            });
     });
 }
 
-inquirer
-  .prompt([
-    {
-        type: "input",
-        message: "What is the ID of the product you would like to buy?",
-        name: "item_id"
-      },
-      {
-        type: "input",
-        message: "How many units of the product would you like to buy?",
-        name: "item_units"
-      }
-  ])
+
+
